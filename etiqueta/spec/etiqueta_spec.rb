60c900f80b8fb35038fabd64d8ff3960df726623
Daniel Rodríguez Suárez <daniel1305972@gmail.com>
2018-11-07 19:05:50 +0000

    Creado el test que instancia la clase, falla

--- etiqueta/spec/etiqueta_spec.rb
+++ etiqueta/spec/etiqueta_spec.rb
@@ -2,6 +2,8 @@ require "spec_helper"
 require 'benchmark'
 
 Spec.describe Info_nutri do
-
+  before :each do
+    @etiqueta = Info_nutri.new("Pollo",1000,5.5,0.5,2,3.5,0.7,5,1.6,2.7,4.8,9,2.3,3.7,2,500)
+  end
 
 end
