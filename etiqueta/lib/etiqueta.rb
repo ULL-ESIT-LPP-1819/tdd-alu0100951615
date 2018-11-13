d7d4960255fbdb59441406a4db2db4f32b99ad3d
Daniel Rodríguez Suárez <alu0100951615@ull.edu.es>
2018-11-13 13:11:51 +0000

    Creados los métodos funcionales de la lista, faltan impresión y recorrerla

--- etiqueta/lib/etiqueta.rb
+++ etiqueta/lib/etiqueta.rb
@@ -1,5 +1,6 @@
 require "etiqueta/version"
 require "etiqueta/Info_nutri.rb"
+require "etiqueta/DLL.rb"
 
 module Etiqueta
   class Error < StandardError; end
---
+++ etiqueta/lib/etiqueta/DLL.rb
@@ -0,0 +1,37 @@
+Node = Struct.new(:value, :next_, :prev)
+
+class DLL
+    
+    attr_reader :head, :tail
+    
+    def initialize(val)
+       @head = Node.new(val,nil,nil)
+       @tail = @head
+    end
+    
+    def insert_tail(val)
+       aux = Node.new(val,nil,@tail)
+       @tail.next_ = aux
+       @tail = aux
+    end
+    
+    def insert_head(val)
+       aux = Node.new(val,@head,nil)
+       @head.prev = aux
+       @head = aux
+    end
+    
+    def extract_head
+       puntero = @head.value
+       @head = @head.next_
+       @head.prev = nil
+       return puntero
+    end
+    
+    def extract_tail
+       puntero = @tail.value
+       @tail = @tail.prev
+       @tail.next_ = nil
+       return puntero
+    end
+end
